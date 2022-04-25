44fafcd7ed51de6cab35e3cc08d1761b22ac120a
SumaiyahH <maya-573@hotmail.co.uk>
2022-04-25 14:28:30 +0000

    attempted Docker API

---
+++ README.md
@@ -0,0 +1,16 @@
+# 6130COMP NotFlix Coursework
+
+## How to make it work on your machine:
+1. Change the Virtual Machine IP address in the mongo.js file (`amqp.connect('amqp://user:bitnami@IP.ADDRESS`) to your IP Address
+2. Install Docker & Docker Compose
+3. Then to build my solution do: `sudo docker-compose build`
+4. To run my solution then do `docker-compose up`
+
+
+## What I have implemented
+- 3 Nodes Tiers - 3 containers
+- Application autom
+
+
+## Not implemented
+- Scale up and scale down
--- nodejs/src/mongo.js
+++ nodejs/src/mongo.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
 
 //To store nodes from each container
-var nodes = [];
+var aliveNodes  = [];
 
 
 var db = mongoose.connection;
@@ -84,14 +84,14 @@ var hasMessageQueueStarted = false;
 // Store list of nodes in the array of the message that contains node identifier, hostname, date and status, 
 var nodeMessage = { nodeIdentifier: nodeIdentifier, hostname: myhostname, date: dateandTime, status: status };
 
-nodes.push(nodeMessage);
+aliveNodes .push(nodeMessage);
 
 
 //Required for messege queueing for RabbitMQ
 var amqp = require('amqplib/callback_api');
 // Node publish Alive message (every 2 seconds)
 setInterval(function () {
-  amqp.connect('amqp://user:bitnami@192.168.56.10', function(error0, connection) {
+  amqp.connect('amqp://user:bitnami@192.168.56.101', function(error0, connection) {
     if (error0) {
       throw error0;
     }
@@ -121,7 +121,7 @@ dateandTime = new Date().getTime() / 1000;
 
 
 //subscribe
-amqp.connect('amqp://user:bitnami@192.168.56.10' , function(error0, connection) {
+amqp.connect('amqp://user:bitnami@192.168.56.101' , function(error0, connection) {
   if (error0) {
     throw error0;
   }
@@ -143,21 +143,23 @@ amqp.connect('amqp://user:bitnami@192.168.56.10' , function(error0, connection) 
       channel.bindQueue(q.queue, exchange, '');
       channel.consume(q.queue, function (msg) {
         if (msg.content) {
+          hasMessageQueueStarted = true;
           console.log(" [x] %s", msg.content.toString());
 
           var newMessage = JSON.parse(msg.content.toString());
           dateandTime = new Date().getTime() / 1000;
           // checks to see if node is in the current list, otherwise list gets updated and node is updated with new node identifier 
-          if (nodes.some(singleNode => singleNode.hostname === newMessage.hostname)) {
-            var nodeThatExists = nodes.find(existingNode => existingNode.hostname === newMessage.hostname);
+          if (aliveNodes .some(singleNode => singleNode.hostname === newMessage.hostname)) {
+            var nodeThatExists = aliveNodes .find(existingNode => existingNode.hostname === newMessage.hostname);
             nodeThatExists.date = dateandTime;
             if (nodeThatExists.nodeIdentifier !== newMessage.nodeIdentifier) {
               nodeThatExists.nodeIdentifier = newMessage.nodeIdentifier;
             }
-            hasMessageQueueStarted = true;
           } else {
-            nodes.push(newMessage);
+            aliveNodes.push(newMessage);
           }
+          console.log("Printing out all alive nodes");
+          console.log(aliveNodes);
         }
       }, {
         noAck: true
@@ -171,7 +173,7 @@ setInterval(function () {
   //Make sure this code doesn't execute if RabbitMQ hasn't started (fixes bug where all nodes are leaders)
   if (hasMessageQueueStarted) {
     var maxNodeIDIdentifier = 0;
-    Object.entries(nodes).forEach(([hostname, prop]) => {
+    Object.entries(aliveNodes ).forEach(([hostname, prop]) => {
       if (prop.hostname != myhostname) {
         if (prop.nodeIdentifier > maxNodeIDIdentifier) {
           maxNodeIDIdentifier = prop.nodeIdentifier;
@@ -187,6 +189,7 @@ setInterval(function () {
 
 
 
+      
 
 app.put('/',  (req, res) => {
   res.send('Got a PUT request at /')
@@ -196,3 +199,114 @@ app.put('/',  (req, res) => {
 app.listen(port, () => {
   console.log(`Express Application listening at port ` + port)
 })
+
+
+
+function containerQty(){
+    request.get({
+    	//we are using the /info url to get the base docker information
+        url: url + "/info",
+    }, (err, res, data) => {
+        if (err) {
+            console.log('Error:', err);
+        } else if (res.statusCode !== 200) {
+	        console.log('Status:', res.statusCode);
+        } else{
+	        //we need to parse the json response to access
+            data = JSON.parse(data)
+            console.log("Number of Containers = " + data.Containers);
+        }
+    });
+}
+
+containerQty();
+
+//import the request library
+var request = require('request');
+
+//This is the URL endopint of your vm running docker
+var url = 'http://192.168.56.101:2375';
+
+
+//this uses the simple get request from request
+
+
+//First attempt to create new container based on week 4 tutorial
+function containerQty(){
+    request.get({
+    	//we are using the /info url to get the base docker information
+        url: url + "/info",
+    }, (err, res, data) => {
+        if (err) {
+            console.log('Error:', err);
+        } else if (res.statusCode !== 200) {
+	        console.log('Status:', res.statusCode);
+        } else{
+	        //we need to parse the json response to access
+            data = JSON.parse(data)
+            console.log("Number of Containers = " + data.Containers);
+        }
+    });
+}
+
+containerQty();
+
+//create the post object to send to the docker api to create a container
+var create = {
+    uri: url + "/v1.40/containers/create",
+	method: 'POST',
+    //deploy an alpine container that runs echo hello world
+	json: {"Image": "alpine", "Cmd": ["echo", "I am Alive"]}
+};
+
+//send the create request
+request(create, function (error, response, createBody) {
+    if (!error) {
+	    console.log("Created container " + JSON.stringify(createBody));
+     
+        //post object for the container start request
+        var start = {
+            uri: url + "/v1.40/containers/" + createBody.Id + "/start",
+	      	method: 'POST',
+	        json: {}
+	    };
+		
+	    //send the start request
+        request(start, function (error, response, startBody) {
+	        if (!error) {
+		        console.log("Container start completed");
+	    
+                //post object for  wait 
+                var wait = {
+			        uri: url + "/v1.40/containers/" + createBody.Id + "/wait",
+                    method: 'POST',
+		            json: {}
+		        };
+		   
+                
+			    request(wait, function (error, response, waitBody ) {
+			        if (!error) {
+				        console.log("run wait complete, container will have started");
+			            
+                        //send a simple get request for stdout from the container
+                        request.get({
+                            url: url + "/v1.40/containers/" + createBody.Id + "/logs?stdout=1",
+                            }, (err, res, data) => {
+                                    if (err) {
+                                        console.log('Error:', err);
+                                    } else if (res.statusCode !== 200) {
+                                        console.log('Status:', res.statusCode);
+                                    } else{
+                                        //we need to parse the json response to access
+                                        console.log("Container stdout = " + data);
+                                        containerQty();
+                                    }
+                                });
+                        }
+		        });
+            }
+        });
+
+    }   
+});
+
